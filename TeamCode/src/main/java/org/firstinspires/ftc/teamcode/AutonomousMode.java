8bd0f7e684a409f679a3f14918672b693d77d860
theawesomew <theawesomew@gmail.com>
2021-04-19 08:20:12 +1000

    Fixed StrafeByDistance function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -11,14 +11,14 @@ import org.firstinspires.ftc.teamcode.robot.XDrive;
 public class AutonomousMode extends OpMode {
     private MotorMap motorMap;
     private XDrive xDrive;
-    private double[] driveValues = {1000, Math.PI/2, 1000, Math.PI, 0, 0};
-    private int iteration = 0;
+    private boolean hasMoved[] = {false, false, false, false};
 
     @Override
     public void init() {
         motorMap = new MotorMap(hardwareMap, "forwardLeft","forwardRight","backLeft","backRight");
         for (DcMotor motor : motorMap.GetMotorMap().values()) {
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             telemetry.addData(motor.toString(), motor.getCurrentPosition());
         }
         xDrive = new XDrive(motorMap);
@@ -36,11 +36,18 @@ public class AutonomousMode extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addData("Drive Distance", driveValues[iteration]);
-        telemetry.addData("Drive Angle", driveValues[iteration+1]);
-        telemetry.addData("Iteration", iteration);
-        if (iteration < driveValues.length-1) {
-            iteration = xDrive.StrafeByDistance(driveValues[iteration], driveValues[iteration + 1], iteration, telemetry);
+        if(!hasMoved[0]) {
+            telemetry.addData("hasMoved1", hasMoved[0]);
+            hasMoved[0] = xDrive.StrafeByDistance(1000, Math.PI/2, telemetry);
+            telemetry.addData("Move", 1);
+        } else if (!hasMoved[1]) {
+            telemetry.addData("hasMoved1", hasMoved[1]);
+            hasMoved[1] = xDrive.StrafeByDistance(1000, Math.PI, telemetry);
+            telemetry.addData("Move", 2);
+        } else if (!hasMoved[2]) {
+            hasMoved[2] = xDrive.StrafeByDistance(1000, Math.PI/3, telemetry);
+        } else if (!hasMoved[3]) {
+            hasMoved[3] = xDrive.RotateByAngle(Math.PI, true, telemetry);
         }
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/XDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/XDrive.java
@@ -9,13 +9,14 @@ import org.firstinspires.ftc.teamcode.lib.Vector;
 public class XDrive extends DriveBase {
     private DcMotor forwardLeft, forwardRight, backLeft, backRight;
     private double pi = Math.PI;
-    private final double ticksPerWeelRotation = 1440;
+    private final double ticksPerWheelRotation = 1440;
     private final double wheelDiameter = 4*25.4;
     private final double wheelRotationsPerDegree = (((335*Math.sqrt(2))/(wheelDiameter))/360);
-    private final double ticksPerDegree = wheelRotationsPerDegree * ticksPerWeelRotation;
+    private final double ticksPerDegree = wheelRotationsPerDegree * ticksPerWheelRotation;
     private final double ticksPerRadian = ticksPerDegree * 180/pi;
-    private final double ticksPerMM = ticksPerWeelRotation/(2*pi*wheelDiameter);
-    private int counter = 0;
+    private final double ticksPerMM = ticksPerWheelRotation/(pi*wheelDiameter);
+    private int forwardLeftDistance = 0, forwardRightDistance = 0, backLeftDistance = 0, backRightDistance = 0;
+    private boolean motorsMoving = false;
 
     public boolean within (int value, int setValue, int error) {
         if (Math.abs(value-setValue) < error) {
@@ -52,12 +53,8 @@ public class XDrive extends DriveBase {
         backRight.setPower(power);
     }
 
-    public int RotateByAngle (double angle, boolean direction, int iterator, Telemetry telemetry) {
-        if (!(forwardRight.isBusy() && forwardLeft.isBusy() && backRight.isBusy() && backLeft.isBusy())) {
-            forwardLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            forwardRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    public boolean RotateByAngle (double angle, boolean direction, Telemetry telemetry) {
+        if (!motorsMoving) {
 
             int targetPosition = (int) Math.round(ticksPerRadian * angle);
 
@@ -80,15 +77,25 @@ public class XDrive extends DriveBase {
             }
 
             SetRotation(power);
-
-            return iterator+2;
+            motorsMoving = true;
+        } else if (within(forwardLeft.getCurrentPosition(), forwardLeft.getTargetPosition(), 10) &&
+                within(forwardRight.getCurrentPosition(), forwardRight.getTargetPosition(), 10) &&
+                within(backLeft.getCurrentPosition(), backLeft.getTargetPosition(), 10)             &&
+                within(backRight.getCurrentPosition(), backRight.getTargetPosition(), 10)) {
+            motorsMoving = false;
+            return true;
         }
-        return iterator;
+
+        return false;
     }
 
-    public int StrafeByDistance (double distance, double angle, int iterator, Telemetry telemetry) {
-        int forwardLeftDistance = 0, forwardRightDistance = 0, backLeftDistance = 0, backRightDistance = 0;
-        if (!forwardRight.isBusy() && !forwardLeft.isBusy() && !backRight.isBusy() && !backLeft.isBusy()) {
+    public boolean StrafeByDistance (double distance, double angle, Telemetry telemetry) {
+        telemetry.addData("frontLeft", within(forwardLeft.getCurrentPosition(), forwardLeft.getTargetPosition(), 10));
+        telemetry.addData("frontRight", within(forwardRight.getCurrentPosition(), forwardRight.getTargetPosition(), 10));
+        telemetry.addData("backLeft", within(backLeft.getCurrentPosition(), backLeft.getTargetPosition(), 10));
+        telemetry.addData("backRight", within(backRight.getCurrentPosition(), backRight.getTargetPosition(), 10));
+
+        if (!motorsMoving) {
             double theta = angle - pi / 4;
 
             forwardLeftDistance = (int) (-Math.round(distance * Math.cos(theta) * ticksPerMM));
@@ -107,13 +114,16 @@ public class XDrive extends DriveBase {
             backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             SetStrafe(1, angle);
-        } else if ( within(forwardLeft.getCurrentPosition(), forwardLeft.getCurrentPosition()+forwardLeftDistance, 10) &&
-                    within(forwardRight.getCurrentPosition(), forwardRight.getCurrentPosition()+forwardRightDistance, 10) &&
-                    within(backLeft.getCurrentPosition(), backLeft.getCurrentPosition()+backLeftDistance, 10) &&
-                    within(backRight.getCurrentPosition(), backRight.getCurrentPosition()+backRightDistance, 10)
-        ) {
-            return (iterator + 2);
+            motorsMoving = true;
+        } else if ( within(forwardLeft.getCurrentPosition(), forwardLeft.getTargetPosition(), 10) &&
+                    within(forwardRight.getCurrentPosition(), forwardRight.getTargetPosition(), 10) &&
+                    within(backLeft.getCurrentPosition(), backLeft.getTargetPosition(), 10)             &&
+                    within(backRight.getCurrentPosition(), backRight.getTargetPosition(), 10)
+           ) {
+            motorsMoving = false;
+            return true;
         }
-        return iterator;
+
+        return false;
     }
 }
